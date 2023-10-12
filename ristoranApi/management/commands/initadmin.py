046e40ba5cca37d96c1cdd08a3a0cb3930ec791e
PabloR9080 <a19216279@alumnos.uady.mx>
2023-10-11 20:29:03 -0600

    add filter to script

--- ristoranApi/management/commands/initadmin.py
+++ ristoranApi/management/commands/initadmin.py
@@ -5,7 +5,7 @@ class Command(BaseCommand):
     help = 'Create admin account if it does not exist'
 
     def handle(self, *args, **options):
-        if UserAPI.objects.count() == 0:
+        if UserAPI.objects.filter(is_superuser=True).count() == 0:
             UserAPI.objects.create_superuser('root@localhost','admin')
         else:
             print('Admin accounts can only be initialized if no Accounts exist')
